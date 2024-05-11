efe387c87344c277a559152e2b862da0bded0780
Geider Frias <geiderde20@gmail.com>
2024-05-11 01:39:43 -0300

    fix bugs the modal and change logic the modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,10 @@
 import Button from "./Button";
-function Modal ({ name,children,text,submit,close, change}) {
+function Modal ({ name,children,text,submit,close, change,btnName}) {
     return (
         <article>
             <form onSubmit={(e)=> e.preventDefault()} className="w-80 border p-2">
                 <div className="flex justify-between mb-2">
-                    <h3>Crear {name}</h3>
+                    <h3>{name}</h3>
                     <span onClick={close} className="cursor-pointer "> X</span>
                 </div>
                 <div className="flex flex-col items-center gap-2">
@@ -14,14 +14,10 @@ function Modal ({ name,children,text,submit,close, change}) {
                 onChange={change}
                 placeholder="Ingresa nombre"  
                 className="inputText"/>
-                {/* <input type="url" placeholder="Ingresa el Url" className="inputText"/>
-                <select name="" id="">
-                    <option value="">General</option>
-                    <option value="">Finanzas</option>
-                    <option value="">Critomonedas</option>
-                </select> */}
-                {children}
-                <Button name="Agregar" style="bg-blue-400" click={submit} />
+               <div>
+                    <Button name={btnName} style="bg-blue-400" click={submit} />
+                    {children}
+               </div>
                 </div>
             </form>
         </article>
--- src/components/RenderTag.jsx
+++ src/components/RenderTag.jsx
@@ -9,10 +9,14 @@ function RenderTag (){
     const [modal, setModal] = useState(false);
     const handleEditOn =()=> { setEdit(true); }
     const handleEditOff =()=>{ !modal && setEdit(false);};
-    const handleModalOff =()=>{setModal(false)};
     const handleModalOn =()=>{
         setModal(true);
         setMetodo("post");
+    };
+    const handleModalOff =()=>{
+        setModal(false);
+        setButtons(false);
+        setTxt("");
     };
 
     //get && post of tag
@@ -22,43 +26,41 @@ function RenderTag (){
     const [buttons,setButtons] = useState(false);
     const [tagId, setTagId] = useState(0);
     const handleGetTxt=(e)=>{
-        const valu = e.target.value;
-        console.log(valu)
         setTxt(e.target.value);
     }
     const handleTagEvent=()=>{
         let typeMetod = metodo;
-        setTxt("");
         console.log("Tipo de metodo: "+typeMetod);
         if(typeMetod == "post"){
             postTag(txt);
+            setModal(false);
         }
         else if(typeMetod == "put"){
             putTag(tagId,txt);
+            setModal(false);
+            setButtons(false);
+
         }
+        setTxt("");
         console.log(tags);
     }
     //put &&delete
-  
-
     const handleTag = (e)=>{
-        setTxt(e.target.innerText);
-        setTagId(e.target.id);  
-        setButtons(true);
-        // console.log(txt);
-        // console.log(tagId);
-    }
-    const handlePutTag = () =>{
-        if(txt!==""){
+        if(edit){
+            setTxt(e.target.innerText);
+            setTagId(e.target.id);  
             setMetodo("put");
+            setButtons(true);
             setModal(true);
-            setButtons(false);
         }
     }
     const handleDeleteTag = ()=>{
         if(txt!==""){
-            setButtons(false);
             deleteTag(tagId);
+            setModal(false);
+            setButtons(false);
+            setTxt("");
+            
         }
     }
     return (
@@ -66,12 +68,13 @@ function RenderTag (){
             {tags&& tags.map(t =>(
                 <Tag key={t.id} name={t.name} pId={t.id} click={handleTag} />
             ))}
-            {buttons && <Button name="Editar" style="bg-green-400" click={handlePutTag} read={false}/>}
-            {buttons && <Button name="Eiminar" style="bg-red-400" click={handleDeleteTag} read={false}/>}
             {edit && <Button name="+" style="bg-green-400" click={handleModalOn} read={false}/>}
                 <Button name={!edit?"Abrir":"Cerrar"} style="bg-blue-400" click={!edit?handleEditOn:handleEditOff} read={false} />
             {
-                modal && <Modal name="Etiqueta" close={handleModalOff} change={handleGetTxt} submit={handleTagEvent} text={txt}/>
+                modal && 
+                <Modal  name="Etiqueta" close={handleModalOff} change={handleGetTxt} submit={handleTagEvent} text={txt} btnName={buttons?"Editar":"Crear"}>
+                  {buttons && <Button name="Eiminar" style="bg-red-400" click={handleDeleteTag} read={false}/>}
+                </Modal>
             }
         </>
     )
