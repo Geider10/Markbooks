72986dcad34290fc0fb7396d1e2769988b9fbb47
Geider Frias <geiderde20@gmail.com>
2024-05-28 23:02:12 -0300

    change star color

--- src/icons/Star.jsx
+++ src/icons/Star.jsx
@@ -1,18 +1,16 @@
-import { useState,useContext} from "react";
+import { useState, useContext } from "react";
 import { FilterContext } from "../context/dataContext";
-function Star ({pId, pStar}){
-    const {changeStar} = useContext(FilterContext);
-    const [color, setColor] = useState(pStar);
-    let status = color;
-    const handleClick=()=>{
-        changeStar(pId, );
-        setTimeout(() => {
-            setColor(!color);
-        }, 1000);
+function Star({ pId, pStar}) {
+    const { changeStar } = useContext(FilterContext);
+    const [color, setColor] = useState(pStar? true: false);
+    const handleClick = () => {
+        setColor(!color)
+        changeStar(pId,!color);
     }
-    return(
-        <svg onClick={handleClick} xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill={color? "#ffee00":"none"}  stroke="#ffffff"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-star">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
+    return (
+        <svg onClick={handleClick}xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={color == true? "#ffee00" : "none"} stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-star" >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
+        </svg>
     )
 }
 export default Star;
