97f3cd89aecc22d5688346ed83061610bdecb90d
Geider Frias <geiderde20@gmail.com>
2024-09-27 00:13:39 -0300

    change content data

--- src/components/main/home/Logo.jsx
+++ src/components/main/home/Logo.jsx
@@ -5,7 +5,7 @@ function Logo (){
             <div className="w-[150px] h-[120px] sm:h-[100px] ">
                 <img src="/logo_book_2.webp" alt="es un libro antiguo representa el logo de la pagina" className="w-full h-[100%]" />
             </div>
-            <h1 className='text-3xl font-bold'>Link.System</h1>
+            <h1 className='text-3xl font-bold'>Link Book</h1>
             <p className="text-slate-300/75 text-xl sm:text-center">Almacena tus enlaces valiosos de manera fácil.</p>
             <div className="flex gap-4 mt-2 items-center text-center">
                 <Path path={"/enlaces"} name={"Ver enlace"} style={"rounded-md px-2 py-1 backOne text-gray-700"}/>
